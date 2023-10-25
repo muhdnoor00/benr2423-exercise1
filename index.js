8c72cf44696b156714018fb3f2844610de2c5ede
muhdnoor00 <b022210217@student.utem.edu.my>
2023-10-25 11:29:19 +0800

    Learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.use(express.json())
-app.get('/', (req, res) => {
-res.send('Hello World!')
+
+app.get('/hello', (req, res) => { // req = request, res = response, send a GET request
+res.send('Hello World!')    // send Hello World as a response
 })
+
+/*app.post('/api/echo', (req, res) => {
+
+}) */
+
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
