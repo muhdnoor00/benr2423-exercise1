162a78a991e932bcfa1b38627440c661741f6f80
muhdnoor00 <b022210217@student.utem.edu.my>
2023-11-07 12:01:14 +0800

    7/11/23

--- indexmongoDB.js
+++ indexmongoDB.js
@@ -2,6 +2,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://b022210217:Meg04fEK7vmuXK0h@class0.qzwsbgr.mongodb.net/?retryWrites=true&w=majority";
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,8 +26,19 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
+app.post('/register', (req, res) => {
+  client.db("Starting").collection("users").insertOne({
+    "username":req.body.username,
+    "password":req.body.password
+  })
+  res.send('Berjaya')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  })
